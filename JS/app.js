1ea7b076f37ffd4f3c3476bd6d25ad3744e167ea
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2021-08-25 14:28:43 +0600

    small update

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,21 @@
 //Customization cost display
 function customizationCost(customizeFeature, customizationCostDisplay, customizationCost) {
     document.getElementById(customizeFeature).addEventListener('click', function () {
-        document.getElementById(customizationCostDisplay).innerHTML = customizationCost;
+        document.getElementById(customizationCostDisplay).innerText = customizationCost;
         calculateTotalCost();
     });
 }
 
 //calculate total cost for different configuration
 function calculateTotalCost() {
-    const bestPrice = parseInt(document.getElementById('best-price').innerHTML);
-    const extraMemoryCost = parseInt(document.getElementById('extra-memory-cost').innerHTML);
-    const extraSsdCost = parseInt(document.getElementById('extra-ssd-cost').innerHTML);
-    const extraDeliveryCost = parseInt(document.getElementById('delivery-cost').innerHTML);
+    const bestPrice = parseInt(document.getElementById('best-price').innerText);
+    const extraMemoryCost = parseInt(document.getElementById('extra-memory-cost').innerText);
+    const extraSsdCost = parseInt(document.getElementById('extra-ssd-cost').innerText);
+    const extraDeliveryCost = parseInt(document.getElementById('delivery-cost').innerText);
     const totalPrice = bestPrice + extraMemoryCost + extraSsdCost + extraDeliveryCost;
 
-    document.getElementById('total-cost').innerHTML = totalPrice;
-    document.getElementById('total-cost-with-pomo').innerHTML = totalPrice;
+    document.getElementById('total-cost').innerText = totalPrice;
+    document.getElementById('total-cost-with-pomo').innerText = totalPrice;
 }
 
 customizationCost('8-gb-memory', 'extra-memory-cost', 0);
@@ -30,9 +30,9 @@ customizationCost('fast-delivery', 'delivery-cost', 20);
 document.getElementById('pomo-btn').addEventListener('click', function () {
     const pomoCode = document.getElementById('pomo-code').value.toLowerCase();
     if (pomoCode == 'stevekaku') {
-        const totalPriceWithoutPomo = parseInt(document.getElementById('total-cost-with-pomo').innerHTML);
+        const totalPriceWithoutPomo = parseInt(document.getElementById('total-cost-with-pomo').innerText);
         const totalPriceWithPomo = totalPriceWithoutPomo - (totalPriceWithoutPomo / 5);
-        document.getElementById('total-cost-with-pomo').innerHTML = totalPriceWithPomo;
+        document.getElementById('total-cost-with-pomo').innerText = totalPriceWithPomo;
         document.getElementById('pomo-code').value = '';
         document.getElementById('pomo-btn').setAttribute('disabled', true);
     }
